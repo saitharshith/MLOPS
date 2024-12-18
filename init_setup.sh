d01da8a29bfd89e8decca04c808fdec432ed0d92
codingbot <sai1726791@gmail.com>
2024-12-18 14:45:53 +0530

    creating env

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+#creating a virtual environment for jupyter notebook
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.8 version" 
+conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "activating the environment" 
+source activate ./env
+echo [$(date)]: "installing the dev requirements" 
+pip install -r requirements.txt
+echo [$(date)]: "END" 
