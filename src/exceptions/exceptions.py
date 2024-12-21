43ee3c31a2df470b39df5f6295d27cdc7d74de35
codingbot <sai1726791@gmail.com>
2024-12-21 17:14:12 +0530

    error handling

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -5,15 +5,17 @@ class custom_exception(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
         _,_,exe_tb=error_details.exc_info()
+        print(exe_tb)
+        
         self.lineno=exe_tb.tb_lineno
         self.file_name=exe_tb.tb_frame.f_code.co_filename
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,self.error_message)
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
 
 if __name__ == '__main__':
     try:
         a=1/0
         
-    except custom_exception as e:
+    except Exception as e:
         raise custom_exception(e,sys)
         
