6fc9f3d85a1ce2d75c20bd98ee331b23ba2b6a67
codingbot <sai1726791@gmail.com>
2024-12-22 16:10:00 +0530

    mlflow integrated

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -12,16 +12,35 @@ import logging
 from src.exceptions.exceptions import custom_exception
 @dataclass
 class ModelEvaluationConfig:
-#configuration for data ingestion
     pass
 
 class ModelEvaluation:
-#data ingestion
     def __init__(self):
         pass
-    def initiate_model_eval(self):
+    def eval_metrics(self,actual,pred):
+        r2=r2_score(actual,pred)
+        mae=mean_absolute_error(actual,pred)
+        mse=mean_squared_error(actual,pred)
+        return mae, mse,r2
+
+    def initiate_model_eval(self,test_array,train_array):
         try:
-            pass
+            x_test,y_test=(test_array[:,:-1],test_array[:,-1])
+            model_path= os.path.join('artifacts','model.pkl')
+            model=load_object(model_path)
+            
+            # mlflow.set_registry_uri("")
+            
+            tracking_url=urlparse(mlflow.get_registry_uri()).scheme
+            print(tracking_url)
+            with mlflow.start_run():
+                predictions=model.predict(x_test)
+                mae,mse,r2=self.eval_metrics(y_test,predictions)
+                mlflow.log_metric("mean squared error",mse)
+                mlflow.log_metric("mean absolute error",mae)
+                mlflow.log_metric("r2 score",r2)
+                mlflow.sklearn.log_model(model,"model",registered_model_name="model_v1")
+                
         except Exception as e:
             logging.info()
             raise custom_exception(sys,e)
